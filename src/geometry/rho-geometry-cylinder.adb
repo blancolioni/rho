73ddf821f9b5ec24f18d28d9587cea2916fbaea0
Fraser Wilson <fraser@thewhitelion.eu>
2021-04-10 17:47:25 +0200

    Update rho-geometry-cylinder.adb
    Added caps

--- src/geometry/rho-geometry-cylinder.adb
+++ src/geometry/rho-geometry-cylinder.adb
@@ -32,8 +32,60 @@ package body Rho.Geometry.Cylinder is
       ------------------
 
       procedure Generate_Cap (Top : Boolean) is
+         Center_Index_Start : constant Vertex_Index := Index;
+         Center_Index_End   : constant Vertex_Index :=
+                                Center_Index_Start
+                                  + Vertex_Index (Radial_Segments);
+         Radius             : constant Real :=
+                                (if Top then Top_Radius else Bottom_Radius);
+         Sign               : constant Real :=
+                                (if Top then 1.0 else -1.0);
       begin
-         null;
+         for X in 1 .. Radial_Segments loop
+            Geometry.Vertex (0.0, Half_Height * Sign, 0.0);
+            Geometry.Normal (0.0, Sign, 0.0);
+            Geometry.Texture (0.5, 0.5);
+            Index := Index + 1;
+         end loop;
+
+         pragma Assert (Index = Center_Index_End);
+
+         for X in 0 .. Radial_Segments loop
+            declare
+               U : constant Real := Real (X) / Real (Radial_Segments);
+               Theta     : constant Rho.Trigonometry.Angle :=
+                             Rho.Trigonometry.From_Degrees
+                               (U * Theta_Length + Theta_Start);
+               Sin_Theta : constant Signed_Unit_Real :=
+                             Rho.Trigonometry.Sin (Theta);
+               Cos_Theta : constant Signed_Unit_Real :=
+                             Rho.Trigonometry.Cos (Theta);
+            begin
+               Geometry.Vertex (Radius * Sin_Theta,
+                                Half_Height * Sign,
+                                Radius * Cos_Theta);
+               Geometry.Normal (0.0, Sign, 0.0);
+               Geometry.Texture (Cos_Theta * 0.5 + 0.5,
+                                 Sin_Theta * 0.5 * Sign + 0.5);
+               Index := Index + 1;
+            end;
+         end loop;
+
+         for X in 1 .. Radial_Segments loop
+            declare
+               C : constant Vertex_Index :=
+                     Center_Index_Start + Vertex_Index (X) - 1;
+               I : constant Vertex_Index :=
+                     Center_Index_End + Vertex_Index (X) - 1;
+            begin
+               if Top then
+                  Geometry.Face (I, I + 1, C);
+               else
+                  Geometry.Face (I + 1, I, C);
+               end if;
+            end;
+         end loop;
+
       end Generate_Cap;
 
       --------------------
