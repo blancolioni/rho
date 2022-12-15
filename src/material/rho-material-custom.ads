991dfbc9a383ea1d64c46467193cbb85826b3ffd
Fraser Wilson <fwilson@anago.nl>
2022-12-15 15:02:30 +0100

    Added package Rho.Material.Custom

---
+++ src/material/rho-material-custom.ads
@@ -0,0 +1,9 @@
+with Rho.Shaders.Stages;
+
+package Rho.Material.Custom is
+
+   function Create_Custom_Material
+     (Shaders : Rho.Shaders.Stages.Shader_Array)
+      return Material_Type;
+
+end Rho.Material.Custom;
